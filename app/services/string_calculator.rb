9b0dfea76a110e3de489f9d593c471b824baa653
KaustubhK711 <43452300+KaustubhK711@users.noreply.github.com>
2025-10-27 17:09:15 +0530

    feat: handled parsing and negative numbers exception

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -6,4 +6,21 @@ class StringCalculator
     check_for_negatives(nums)
     nums.sum
   end
+
+  private
+
+  def parse_numbers(numbers)
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      header, numbers = numbers.split("\n", 2)
+      custom_delimiter = header[2..]
+      delimiter = Regexp.new(Regexp.escape(custom_delimiter))
+    end
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def check_for_negatives(nums)
+    negatives = nums.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
+  end
 end
