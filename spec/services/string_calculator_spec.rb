4b366c9473cd22466991cc0e77dd856c9f060a88
KaustubhK711 <43452300+KaustubhK711@users.noreply.github.com>
2025-10-27 17:16:02 +0530

    feat: added specs for newline and custom delimiter

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -19,6 +19,14 @@ RSpec.describe StringCalculator do
     it 'returns sum for multiple numbers' do
       expect(calculator.add("1,2,3,4")).to eq(10)
     end
+
+    it 'handles newlines between numbers' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiter' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
 
