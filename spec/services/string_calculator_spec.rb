e49bdad5ce48e42f2ce1772456692a1eaa86f1d2
KaustubhK711 <43452300+KaustubhK711@users.noreply.github.com>
2025-10-27 17:13:59 +0530

    feat: added specs for multiplier numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe StringCalculator do
     it 'returns sum for two comma-separated numbers' do
       expect(calculator.add("1,5")).to eq(6)
     end
+
+    it 'returns sum for multiple numbers' do
+      expect(calculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
 
