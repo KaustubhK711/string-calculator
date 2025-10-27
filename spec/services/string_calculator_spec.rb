369230f26066e9ab85430aeffab55e4f3f8b0fc4
KaustubhK711 <43452300+KaustubhK711@users.noreply.github.com>
2025-10-27 17:13:16 +0530

    feat: added specs for empty-string, single digit number and comma seperated positive numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -1,1 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe StringCalculator do
+  describe '#add' do
+    let(:calculator) { described_class.new }
+
+    it 'returns 0 for empty string' do
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for single number' do
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'returns sum for two comma-separated numbers' do
+      expect(calculator.add("1,5")).to eq(6)
+    end
+  end
+end
 
